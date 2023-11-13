5023c6e51b08b40e712b7fc31c4141fdb0e77c74
alansales <alansales00@gmail.com>
2023-11-13 19:59:30 -0300

    First Console App

---
+++ firstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+    }
+}
